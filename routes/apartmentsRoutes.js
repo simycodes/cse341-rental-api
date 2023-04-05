dda7884aa4bbba43265e5aa12fb6687496b31baa
codingxperience <oko22002@byui.edu>
2023-04-05 16:00:17 +0300

    added apartmentsRoutes, & housesRoutes test suites

--- routes/apartmentsRoutes.js
+++ routes/apartmentsRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 // GET THE USER AUTHENTICATION FUNCTION
 const { authenticateUser } = require('../middleware/authentication.js');
 // CREATING-DEFINING THE ROUTER VARIABLE
-const apartmentRouter = express.Router();
+const router = express.Router();
 
 // GET THE VALIDATION FUNCTIONS
 const {
@@ -20,21 +20,16 @@ const {
 } = require('../controllers/apartmentsController.js');
 
 // GET ALL APARTMENTS
-apartmentRouter.get('/', getAllApartments);
+router.get('/', getAllApartments);
 
 // GET AS SINGLE APARTMENT
-apartmentRouter.get('/:id', getSingleApartment);
+router.get('/:id', getSingleApartment);
 
 // ADD AN APARTMENT
-apartmentRouter.post(
-  '/',
-  authenticateUser,
-  validateIncomingApartmentDataForAddApartment,
-  addApartment
-);
+router.post('/', authenticateUser, validateIncomingApartmentDataForAddApartment, addApartment);
 
 // UPDATE AN APARTMENT
-apartmentRouter.put(
+router.put(
   '/:id',
   authenticateUser,
   validateIncomingApartmentDataForUpdatingApartment,
@@ -42,6 +37,6 @@ apartmentRouter.put(
 );
 
 // DELETE AN APARTMENT
-apartmentRouter.delete('/:id', authenticateUser, deleteApartment);
+router.delete('/:id', authenticateUser, deleteApartment);
 
-module.exports = apartmentRouter;
+module.exports = router;
